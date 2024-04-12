de7a3a0856a1ff468122a9a14b32a1a84e9c31fd
John Williams <jrw+3@mit.edu>
2024-04-12 20:48:03 +0000

    serper

--- functions/serpersearch.js
+++ functions/serpersearch.js
@@ -1,10 +1,12 @@
+import process from 'process';
 
 const execute = async (query) => {
     let top_result_to_return = 4
     let url = "https://google.serper.dev/search"
-    let payload = json.dumps({"q": query})
-    headers = {
-    'X-API-KEY': os.environ['SERPER_API_KEY'],
+    let payload = JSON.stringify({"q": query})
+    let serperkey = process.env.SERPER_API_KEY;
+    let headers = {
+    'SERPER-API-KEY': serperkey,
     'content-type': 'application/json'
     }
 // use fetch
@@ -16,7 +18,7 @@ const execute = async (query) => {
         })
         let data = await response.json();
 
-        return data.results.slice(0, top_result_to_return);
+        return data;
     }
     catch (error) {
         console.log(error);
@@ -24,7 +26,7 @@ const execute = async (query) => {
     }
 }
 const details = {
-    "name": "searchweb",
+    "name": "serpersearch",
     "parameters": {
       "type": "object",
       "properties": {
